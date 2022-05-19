1b418140f05568fc359fbdf9ba18eaddd8ecab33
saichandrasekar <saichandrasekar@gmail.com>
2022-05-19 21:34:03 +0530

    initial draft

---
+++ src/main/java/org/mayuran/dataingestor/DataIngestorApplication.java
@@ -0,0 +1,13 @@
+package org.mayuran.dataingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataIngestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mayuran/dataingestor/DataIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mayuran.dataingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
